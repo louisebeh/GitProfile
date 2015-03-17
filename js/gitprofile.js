3767a91c0ee9bce7b1737f3a04e5c0a39bd8ef45
Louise Beh <louise.beh@gmail.com>
2015-03-17 14:38:41 +0000

    Version 0 gitprofile utilising github api

---
+++ js/gitprofile.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $('.gitprofile').on('submit', function(e) {
+
+    e.preventDefault();
+
+    var url = 'https://api.github.com/users/' + $('input.username').val() + '?access_token='+key;
+
+    var template = $('template').html();
+
+    var info = $.get(url, function(info) {
+      $('.container').prepend(Mustache.render(template, info));
+    }).fail(function() {gi
+      $('.container').prepend("User not found")
+    }).always(function() {
+      $('input.username').val('');
+      });
+  });
+});
