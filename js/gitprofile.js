251e5ac19192ff71419e78a92ff0315ef29d2a7e
Louise Beh <louise.beh@gmail.com>
2015-03-17 17:05:20 +0000

    Added V1 so far

--- js/gitprofile.js
+++ js/gitprofile.js
@@ -8,9 +8,9 @@ $(document).ready(function() {
 
     var template = $('template').html();
 
-    var info = $.get(url, function(info) {
+    var info =$.get(url, function(info) {
       $('.container').prepend(Mustache.render(template, info));
-    }).fail(function() {gi
+    }).fail(function() {
       $('.container').prepend("User not found")
     }).always(function() {
       $('input.username').val('');
---
+++ test/gitUserSearchController.spec.js
@@ -0,0 +1,20 @@
+describe('GitUserSaearchController', function() {
+  beforeEach(module('GitUserSearch'));
+
+  var scope, ctrl;
+
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    ctrl = $controller('GitUserSearchController', {
+      $scope: scope
+
+    it('should initialise with an empty search result and term', function(){
+      expect(scope.searchResult).toBeUndefined();
+      expect(scope.searchTerm).toBeUndefined();
+    });
+
+
+    });
+
+  }));
+});
